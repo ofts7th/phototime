a35e43befd7212424babbdc55a817d851ee13c26
sunliang <ofts7th@163.com>
2018-08-01 12:26:44 +0800

    Add Time form

---
+++ AddTime.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace photoTimer
+{
+    partial class AddTime
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(12, 12);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(294, 21);
+            this.txtFolder.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(312, 12);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(312, 55);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess.TabIndex = 2;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(12, 55);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(294, 73);
+            this.txtResult.TabIndex = 3;
+            // 
+            // AddTime
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 364);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFolder);
+            this.Name = "AddTime";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AddTime";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
---
+++ AddTime.cs
@@ -0,0 +1,76 @@
+﻿using photoTimer.lib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace photoTimer
+{
+    public partial class AddTime : Form
+    {
+        public AddTime()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (this.folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.txtFolder.Text = this.folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            processFolder(this.txtFolder.Text, this.txtFolder.Text);
+        }
+
+        private void processFolder(string rootFolder, string folder)
+        {
+            foreach (string sd in Directory.GetDirectories(folder))
+            {
+                processFolder(rootFolder, sd);
+            }
+            foreach (string f in Directory.GetFiles(folder))
+            {
+                processFile(rootFolder, f);
+            }
+        }
+
+        private void processFile(string rootFolder, string file)
+        {
+            Image image = Image.FromFile(file);
+            string newFile = rootFolder + "\\new" + file.Substring(rootFolder.Length);
+            string newDir = Path.GetDirectoryName(newFile);
+            if (!Directory.Exists(newDir))
+            {
+                Directory.CreateDirectory(newDir);
+            }
+            try
+            {
+                var timePropertyItem = image.GetPropertyItem(0x9003);
+                if (timePropertyItem != null)
+                {
+                    string text = Encoding.ASCII.GetString(timePropertyItem.Value);
+                    if (!string.IsNullOrEmpty(text))
+                    {
+                        if (text.IndexOf(' ') > 0)
+                        {
+                            text = text.Substring(0, text.IndexOf(' '));
+                        }
+                        text = text.Replace(':', '-');
+
+                        WaterMarkHelper.AddImageSignText(file, newFile, text, 9, 80, 60, Color.OrangeRed);
+                    }
+                }
+            }
+            catch (Exception ex) { }
+        }
+    }
+}
